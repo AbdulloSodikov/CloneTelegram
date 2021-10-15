049325cd485adc333107953e584e27b5b0fcc0c1
AbdulloSodikov <sodikov.ak@gmail.com>
2021-10-15 16:13:55 +0500

    add Header

--- app/src/main/java/com/ak/sodikov/clonetelegram/MainActivity.kt
+++ app/src/main/java/com/ak/sodikov/clonetelegram/MainActivity.kt
@@ -2,14 +2,49 @@ package com.ak.sodikov.clonetelegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.ak.sodikov.clonetelegram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
-private lateinit var mBinding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityMainBinding
+    private lateinit var mDrawer : Drawer
+    private lateinit var mHeader :  AccountHeader
+    private lateinit var mToolbar : Toolbar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
     }
-}s
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar  // Инициализация ToolBar
+        initFunction ()
+    }
+
+    private fun initFunction() {
+       setSupportActionBar(mToolbar) // Передача  ToolBar
+
+    }
+    private fun createHeader(){
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Абдулло Содиков")
+                    .withEmail("+992918865861")
+            ).build()
+        }
+    }
